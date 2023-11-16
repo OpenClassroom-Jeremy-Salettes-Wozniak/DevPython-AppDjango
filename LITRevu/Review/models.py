d8d1de05db6f33315a2362313ae823a1022f7197
Jérémy Salettes-Wozniak "AlchTech <HarmonyFidelis@gmail.com>
2023-11-16 11:26:47 +0100

    Correction des erreurs suite au lancement du serveur

--- LITRevu/Review/models.py
+++ LITRevu/Review/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
-
+from django.contrib.auth.models import User
 # Create your models here.
 class Review(models.Model):
     review_id = models.AutoField(primary_key=True)
-    review_ticket = models.ForeignKey('Ticket', on_delete=models.CASCADE)
+    review_ticket = models.ForeignKey('ticket.Ticket', on_delete=models.CASCADE)
     review_rating = models.PositiveSmallIntegerField(max_length=1024, validators=[MinValueValidator(0), MaxValueValidator(5)])
-    review_user = models.ForeignKey('User', on_delete=models.CASCADE)
+    review_user = models.ForeignKey(User, on_delete=models.CASCADE)
     review_headline = models.CharField(max_length=128)
     review_body = models.CharField(max_length=8192)
     review_time_created = models.DateTimeField(auto_now_add=True)
--- LITRevu/User/views.py
+++ LITRevu/User/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
+class index(View):
+    def get(self, request):
+        return render(request, 'user/index.html')
+
+class register(View):
+    def get(self, request):
+        return render(request, 'user/register.html')
+    
